6d1aea3487bb7b7be895dfa6953cf355d39d9df3
JosipGoluza <josip.goluza12@gmail.com>
2023-03-28 08:50:33 +0200

    Create retrofit instance and API model

---
+++ app/src/main/java/com/example/kunaify/model/ExhangeRateAPI.kt
@@ -0,0 +1,5 @@
+package com.example.kunaify.model
+
+data class ExhangeRateAPI(
+    val result: Double,
+)
---
+++ app/src/main/java/com/example/kunaify/networking/RetrofitInstance.kt
@@ -0,0 +1,33 @@
+package com.example.kunaify.networking
+
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+class RetrofitInstance {
+
+    companion object {
+        val BASE_URL = "https://api.apilayer.com/exchangerates_data"
+
+        val interceptor = HttpLoggingInterceptor().apply {
+            this.level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        val client = OkHttpClient.Builder().apply {
+            this.addInterceptor(interceptor)
+                .connectTimeout(20, TimeUnit.SECONDS)
+        }.build()
+
+        fun getRetrofitInstance(): Retrofit {
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+                .build()
+        }
+    }
+
+}
